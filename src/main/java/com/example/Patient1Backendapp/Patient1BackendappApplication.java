3a981f3b8214a4481098f36ebe40b9c0c51cfa61
jaseel <jaseeljctcs@gmail.com>
2022-12-06 21:59:20 +0530

    initial

---
+++ src/main/java/com/example/Patient1Backendapp/Patient1BackendappApplication.java
@@ -0,0 +1,13 @@
+package com.example.Patient1Backendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patient1BackendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Patient1BackendappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Patient1Backendapp/Patient1BackendappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Patient1Backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patient1BackendappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
