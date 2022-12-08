1492b927a0281899af8616c46c7dfce0a01d593b
jaseel <jaseeljctcs@gmail.com>
2022-12-08 14:54:32 +0530

    dao

--- src/main/java/com/example/Patient1Backendapp/Controller/PatientController.java
+++ src/main/java/com/example/Patient1Backendapp/Controller/PatientController.java
@@ -3,30 +3,49 @@ package com.example.Patient1Backendapp.Controller;
 import com.example.Patient1Backendapp.Dao.PatientDao;
 import com.example.Patient1Backendapp.Model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 public class PatientController {
     @Autowired
     private PatientDao dao;
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
 
     public  String Home(){
         return "home page";
     }
-    @PostMapping(path = "/add",consumes ="application/json",produces = "application/json")
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/addpatient",consumes ="application/json",produces = "application/json")
     public String Add(@RequestBody Patient p){
-        System.out.println(p.getPatient_name().toString());
+        System.out.println(p.getPatient_name());
         dao.save(p);
-        return "Employee add page";
+        return "Patient add successfully";
     }
-    @GetMapping("/view")
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewpatient")
     public List<Patient> View(){
         return (List<Patient> ) dao.findAll();
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Patient> Search(@RequestBody Patient p){
+        System.out.println(p.getPatient_id());
+        return (List<Patient>) dao.Patientsearch(p.getPatient_id());
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete",consumes = "application/json",produces = "application/json")
+    public HashMap<String, String> Delete(@RequestBody Patient p){
+        String patientid=String.valueOf(p.getId());
+        System.out.println(patientid);
+        dao.Deletepatient(p.getId());
+        HashMap<String,String> map =new HashMap<>();
+        map.put("status","success");
+        return  map;
+
+
     }
 }
--- src/main/java/com/example/Patient1Backendapp/Dao/PatientDao.java
+++ src/main/java/com/example/Patient1Backendapp/Dao/PatientDao.java
@@ -1,7 +1,21 @@
 package com.example.Patient1Backendapp.Dao;
 
 import com.example.Patient1Backendapp.Model.Patient;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PatientDao extends CrudRepository<Patient,Integer> {
+
+    @Query(value = "SELECT `id`, `address`, `contact_no`, `doc_name`, `patient_id`, `patient_name`, `addres` FROM `patient` WHERE `patient_id` =:patient_id ",nativeQuery = true)
+    List<Patient> Patientsearch(@Param("patient_id") Integer patient_id);
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `patient` WHERE `id` =:id ",nativeQuery = true)
+    void Deletepatient(@Param("id") Integer id);
+
 }
